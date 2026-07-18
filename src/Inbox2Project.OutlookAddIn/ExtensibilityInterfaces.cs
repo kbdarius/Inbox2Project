f2bd520a34ad354f31d5cc11db054d950f79afc6
Keivan Darius <Admin@zodvest.com>
2026-07-18 15:01:51 -0400

    fix: use dispatch-compatible Outlook add-in ABI

--- src/Inbox2Project.OutlookAddIn/ExtensibilityInterfaces.cs
+++ src/Inbox2Project.OutlookAddIn/ExtensibilityInterfaces.cs
@@ -16,12 +16,21 @@ public enum ExtensibilityDisconnectMode
 
 [ComVisible(true)]
 [Guid("B65AD801-ABAF-11D0-BB8B-00A0C90F2744")]
-[InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+[InterfaceType(ComInterfaceType.InterfaceIsDual)]
 public interface IDTExtensibility2
 {
+    [DispId(1)]
     void OnConnection(object application, ExtensibilityConnectMode connectMode, object addInInstance, ref Array custom);
+
+    [DispId(2)]
     void OnDisconnection(ExtensibilityDisconnectMode removeMode, ref Array custom);
+
+    [DispId(3)]
     void OnAddInsUpdate(ref Array custom);
+
+    [DispId(4)]
     void OnStartupComplete(ref Array custom);
+
+    [DispId(5)]
     void OnBeginShutdown(ref Array custom);
 }
